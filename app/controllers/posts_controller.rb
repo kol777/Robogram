6e52bd7f73a55fc7e834b9d8908f272e32ab82f8
flavius <flavius@localhost.localdomain>
2017-08-17 07:48:21 -0700

    Finished CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,36 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
+    if @post = Post.create(post_params)
+      flash[:success] = 'Post created successfully!'
+      redirect_to posts_path
+    else
+      flash.now[:alert] = 'Something went wrong, please recheck your form!'
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:success] = 'Post updated successfully!'
+      redirect_to(posts_path(@post))
+    else
+      flash.now[:alert] = 'Update failed! Recheck your form!'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
     redirect_to posts_path
   end
 
@@ -16,5 +45,5 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:image, :caption)
   end
-  
+
 end
